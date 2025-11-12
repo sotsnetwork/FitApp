b6841208a4ba968b2b71635980b7f315cd94bfb8
Chidera Nwachukwu <chideranwachukwu32@gmail.com>
2025-11-12 14:00:54 +0100

    Update home.tsx

--- app/(creator)/home.tsx
+++ app/(creator)/home.tsx
@@ -47,7 +47,7 @@ export default function CreatorHome() {
         <View style={{ paddingHorizontal: spacing.lg, marginBottom: spacing.md }}>
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: spacing.sm }}>
             <Text style={{ fontSize: 18, fontFamily: fonts.bold }}>Daily Goal</Text>
-            <TouchableOpacity onPress={() => router.push('/(user)/daily-goals')}>
+            <TouchableOpacity>
               <Text style={{ fontSize: 14, fontFamily: fonts.regular, color: colors.text, opacity: 0.5 }}>See all</Text>
             </TouchableOpacity>
           </View>
@@ -78,7 +78,7 @@ export default function CreatorHome() {
         <View style={{ paddingHorizontal: spacing.lg, marginBottom: spacing.md }}>
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: spacing.sm }}>
             <Text style={{ fontSize: 18, fontFamily: fonts.bold }}>Ongoing Challenges</Text>
-            <TouchableOpacity onPress={() => router.push('/(user)/ongoing-challenges')}>
+            <TouchableOpacity>
               <Text style={{ fontSize: 14, fontFamily: fonts.regular, color: colors.text, opacity: 0.5 }}>See all</Text>
             </TouchableOpacity>
           </View>
